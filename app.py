69942507b59c612919401634f81492962d75f926
rangga <arsyrangga@gmail.com>
2024-12-02 19:38:28 +0700

    fixing

--- app.py
+++ app.py
@@ -110,4 +110,5 @@ async def predict(file: UploadFile = File(...)):
         )
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
